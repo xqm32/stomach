dd9173b78d1e951f1574a5b70f5ddf01438272f0
Qiming Xu <33349132+xqm32@users.noreply.github.com>
2025-07-06 05:04:27 +0800

    feat: add search

---
+++ 1-db.sh
@@ -0,0 +1,3 @@
+#!/bin/bash -e
+
+duckdb -ui ./db/stomach.duckdb
---
+++ search.ts
@@ -0,0 +1,25 @@
+import { QdrantClient } from "@qdrant/js-client-rest";
+import { embed } from "ai";
+import { siliconflow } from "./ai";
+
+const client = new QdrantClient({ url: process.env.QDRANT_URL });
+const collection = "stomach";
+
+async function search(name: string) {
+  const { embedding, usage } = await embed({
+    model: siliconflow.textEmbeddingModel("Qwen/Qwen3-Embedding-0.6B"),
+    value: name,
+  });
+  const result = await client.query(collection, {
+    query: embedding,
+    with_payload: true,
+  });
+  console.log(
+    `Search results for "${name}":`,
+    result.points.map((point) => ({
+      score: point.score,
+      ...point.payload!,
+    }))
+  );
+  return result;
+}
